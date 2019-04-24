7795dcb2591c4206a0bbc4dda908a304f04128fa
Cas Bertrams <cas.bertrams@eproseed.com>
2019-04-24 20:08:58 +0200

    bugfix (srry had m de vorige keer niet gecommit

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
 	   System.out.print("Enter your name: ");
 	   name = user_input.next();
        System.out.println("Hello " + name);
-	   System.out.println("vet belangrijke fix");
+	   System.out.println("vet belangrijke fix: wow opnieuw gefixt wat knap zeg");
 	   System.out.print("HAHAHAHAHA");
     }
 
